62a259d537edc88235b999cb2c74d83b318dc9a7
katsmamina <katsmamina@yandex.ru>
2022-02-14 00:06:19 +0100

    Updates

---
+++ precache-manifest.108ff01f4bc09ac30d659b4279aabf7d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94c0b21db959f14243ca340c68bd6a5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "3ed9cb73fabb2c4e7b57",
+    "url": "/my-portfolio/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "73b05a6087fd13e0583e",
+    "url": "/my-portfolio/static/css/main.4331c200.chunk.css"
+  },
+  {
+    "revision": "3ed9cb73fabb2c4e7b57",
+    "url": "/my-portfolio/static/js/2.093ee680.chunk.js"
+  },
+  {
+    "revision": "e13ee005b20792bf401a8ddd8bd5f6e2",
+    "url": "/my-portfolio/static/js/2.093ee680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972b8a97c69d14d6854f",
+    "url": "/my-portfolio/static/js/3.ee11d966.chunk.js"
+  },
+  {
+    "revision": "73b05a6087fd13e0583e",
+    "url": "/my-portfolio/static/js/main.f51c45d1.chunk.js"
+  },
+  {
+    "revision": "7c0a208edb022fd97cb2",
+    "url": "/my-portfolio/static/js/runtime-main.44b02b12.js"
+  },
+  {
+    "revision": "f63ada20353a0fd2d4c64468b047fe65",
+    "url": "/my-portfolio/static/media/BeHelp.f63ada20.jpeg"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/my-portfolio/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "09e4ec27756a18c936f92f4e2957e822",
+    "url": "/my-portfolio/static/media/css-3.09e4ec27.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/my-portfolio/static/media/git.1190a19c.svg"
+  },
+  {
+    "revision": "7dffd9f3befc41d7496ce325f3055084",
+    "url": "/my-portfolio/static/media/html5.7dffd9f3.svg"
+  },
+  {
+    "revision": "b2b629d0b0a96a0b124bd96763c333e6",
+    "url": "/my-portfolio/static/media/loading.b2b629d0.svg"
+  },
+  {
+    "revision": "ca3520d885bb4fda93f6bc154f85a738",
+    "url": "/my-portfolio/static/media/myphoto.ca3520d8.JPEG"
+  },
+  {
+    "revision": "312699aa50f42ed83a8eda33ab8977dc",
+    "url": "/my-portfolio/static/media/nodejs.312699aa.svg"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/my-portfolio/static/media/react-1.876a8325.svg"
+  },
+  {
+    "revision": "470548b4a95cae966d0750d6720e4082",
+    "url": "/my-portfolio/static/media/sass.470548b4.svg"
+  },
+  {
+    "revision": "22efb7c22774c39c790448f62bda240d",
+    "url": "/my-portfolio/static/media/vuejs.22efb7c2.svg"
+  }
+]);
